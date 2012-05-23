f70e573225eff90dc82dd3459b7b021dd18c49d4
Andy Villiger <a.villiger@gmail.com>
2012-05-23 15:31:59 +0200

    score added

--- src/ch/bbbaden/space/Space.java
+++ src/ch/bbbaden/space/Space.java
@@ -4,12 +4,14 @@ import ch.bbbaden.space.entities.Explosion;
 import ch.bbbaden.space.entities.Entity;
 import ch.bbbaden.space.entities.Meteor;
 import ch.bbbaden.space.entities.Player;
+import com.sun.tools.javac.comp.Enter;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.util.Log;
 import java.util.ArrayList;
+import org.newdawn.slick.Color;
 
 /**
  * @author avi
@@ -17,37 +19,53 @@ import java.util.ArrayList;
 public final class Space implements IGameObject {
     
     private Image mBackgroundImage;
+    private Image mGameOverImage;
     private int mTimeSinceLastMeteor;
     private ArrayList<Entity> mEntities = new ArrayList<Entity>();
+    private Player mPlayer;
+    private int mScore = 0;
     
     public Space() {
         try {
             mBackgroundImage = new Image("images/background0.png");
+            mGameOverImage = new Image("images/gameover.png");
         } catch (SlickException e) {
             Log.error("Fehler: " + e.getMessage());
         }
         
-        addEntity(new Player(Game.SCREEN_WIDTH / 2, Game.SCREEN_HEIGHT - 50, this));
+        mPlayer = new Player(Game.SCREEN_WIDTH / 2, Game.SCREEN_HEIGHT - 50, this);
+        addEntity(mPlayer);
     }
 
     public void update(GameContainer container, int delta) {
         // Update all entities
         for (int i = mEntities.size() - 1; i >= 0; i--) {
             Entity entity = mEntities.get(i);
-            entity.update(container, delta);
             
-            // Flag them as destroyed if they collide with another entity
-            for (int j = mEntities.size() - 1; j >= 0; j--) {
-               Entity other = mEntities.get(j);
-               if (entity.collides(other)) {
-                   entity.destroy();
-                   other.destroy();
-                   
-                   float x = (entity.getX() + other.getX()) / 2.f;
-                   float y = (entity.getY() + other.getY()) / 2.f;
-                   
-                   addEntity(new Explosion(x, y));
-               }
+            if (entity.isDestroyed()) {
+                removeEntity(entity);
+            } else {
+                entity.update(container, delta);
+            
+                // Flag them as destroyed if they collide with another entity
+                for (int j = mEntities.size() - 1; j >= 0; j--) {
+                   Entity other = mEntities.get(j);
+                   if (entity.collides(other)) {
+                       entity.destroy();
+                       other.destroy();
+
+                       float x = (entity.getX() + other.getX()) / 2.f;
+                       float y = (entity.getY() + other.getY()) / 2.f;
+
+                       addEntity(new Explosion(x, y));
+                       
+                       if (entity.getType() == Entity.Type.Meteor || 
+                           entity.getType() == Entity.Type.Meteor) {
+                           // Add score
+                           addScorePoints(entity.getSize());
+                       }
+                   }
+                }
             }
         }
         
@@ -63,7 +81,7 @@ public final class Space implements IGameObject {
         mTimeSinceLastMeteor += delta;
         if (mTimeSinceLastMeteor > Game.TIME_BETWEEN_METEORS) {
             mTimeSinceLastMeteor = 0;
-            addEntity(Meteor.createMeteor());
+            addEntity(Meteor.createMeteor(this));
         }
     }
 
@@ -74,6 +92,14 @@ public final class Space implements IGameObject {
         for (Entity entity : mEntities) {
             entity.render(container, graphics);
         }
+        
+        // If the player is destroyed, we show the game over picture
+        if (mPlayer.isDestroyed()) {
+            mGameOverImage.draw();
+        }
+        
+        graphics.setColor(Color.magenta);
+        graphics.drawString("Score: " + mScore, 9 , 30);
     }
     
     public void addEntity(Entity entity) {
@@ -82,6 +108,10 @@ public final class Space implements IGameObject {
     
     public void removeEntity(Entity entity) {
         mEntities.remove(entity);
+    }
+    
+    public void addScorePoints(int points) {
+        mScore += points;
     }
     
 }
--- src/ch/bbbaden/space/entities/Meteor.java
+++ src/ch/bbbaden/space/entities/Meteor.java
@@ -1,6 +1,7 @@
 package ch.bbbaden.space.entities;
 
 import ch.bbbaden.space.Game;
+import ch.bbbaden.space.Space;
 import java.util.Random;
 import org.newdawn.slick.GameContainer;
 
@@ -10,18 +11,20 @@ import org.newdawn.slick.GameContainer;
 public class Meteor extends Entity {
     
     private float mSpeed;
-    
+    private Space mSpace;
+
     @Override
     public Type getType() {
         return Type.Meteor;
     }
     
-    public Meteor(float x, float y, int which, float speed) {
+    public Meteor(float x, float y, int which, float speed, Space space) {
         super(x, y, "images/meteor" + which + ".png");
         mSpeed = speed;
+        mSpace = space;
     }
     
-    public static Meteor createMeteor() {
+    public static Meteor createMeteor(Space space) {
         Random rand = new Random();
         int x = rand.nextInt(Game.SCREEN_WIDTH - 40) + 20;
         int y = -50;
@@ -30,7 +33,7 @@ public class Meteor extends Entity {
         // Random speed between METEOR_SPEED and METEOR_SPEED + 0.1f
         float speed = rand.nextInt(100) / 1000.f + Game.METEOR_SPEED;
         
-        return new Meteor(x, y, which, speed);
+        return new Meteor(x, y, which, speed, space);
     }
 
     public void update(GameContainer container, int delta) {
@@ -38,8 +41,17 @@ public class Meteor extends Entity {
         
         // Check if Y-position out of screen boundaries
         if (mY > Game.SCREEN_HEIGHT + 50) {
-            destroy();
+            // Don't use the destroy-method, which would add score points
+            mDestroyed = true;
         }
+    }
+    
+    @Override
+    public void destroy() {
+        super.destroy();
+        
+        // Add score points in relation to the size of the meteor
+        mSpace.addScorePoints(100 / mSize);
     }
     
 }
